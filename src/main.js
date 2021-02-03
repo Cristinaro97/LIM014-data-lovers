aae9a3352e1dff0907276590fc570bfcc6442fca
Cristinaro97 <cristinagrl@hotmail.com>
2021-02-03 10:43:38 -0500

    change Cristina

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/athletes/athletes.js';
 //hola soy cristina
-
+//i'm hungry
 console.log(example, data);
